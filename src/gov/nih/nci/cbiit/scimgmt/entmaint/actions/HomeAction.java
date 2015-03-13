80df78db77224f7be8d97904763eed5cdc0a33ca
menons2 <menons2@nih.gov>
2015-03-13 16:43:38 +0000

    Set current audit in session at startup.

--- src/gov/nih/nci/cbiit/scimgmt/entmaint/actions/HomeAction.java
+++ src/gov/nih/nci/cbiit/scimgmt/entmaint/actions/HomeAction.java
@@ -1,6 +1,11 @@
 package gov.nih.nci.cbiit.scimgmt.entmaint.actions;
       
+import gov.nih.nci.cbiit.scimgmt.entmaint.constants.ApplicationConstants;
+import gov.nih.nci.cbiit.scimgmt.entmaint.services.AdminService;
+import gov.nih.nci.cbiit.scimgmt.entmaint.valueObject.EmAuditsVO;
+
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 
@@ -14,10 +19,18 @@ public class HomeAction extends BaseAction {
 	
 	static Logger logger = Logger.getLogger(HomeAction.class);
 	
-	
+	@Autowired
+	AdminService adminService;
 	
     public String execute() throws Exception {
         String forward = SUCCESS;
+        
+        //Retrieve current audit info
+        
+        EmAuditsVO emAuditsVO = adminService.retrieveCurrentAudit();
+        
+        //Store this in the session
+        setAttributeInSession(ApplicationConstants.CURRENT_AUDIT, emAuditsVO);             
 
         return forward;
     }
