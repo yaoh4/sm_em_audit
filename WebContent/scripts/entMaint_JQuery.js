6740befe78edc66686231a508eb4e4b5167a6ffe
tembharend <tembharend@nih.gov>
2015-03-24 19:40:37 +0000

    moving onCategoryChange function here

--- WebContent/scripts/entMaint_JQuery.js
+++ WebContent/scripts/entMaint_JQuery.js
@@ -100,3 +100,14 @@ function onOrgChange(org){
 		$("#excludeNciCheck").attr("disabled","disabled");
 	}		
 }
+
+function onCategoryChage(category){
+	if(category == '22' || category == '25'){
+		$('#dateRangeStartDate').datepicker('disable'); 
+		$('#dateRangeEndDate').datepicker('disable'); 		
+	}
+	else{
+		$('#dateRangeStartDate').datepicker('enable'); 
+		$('#dateRangeEndDate').datepicker('enable'); 
+	}		
+}
