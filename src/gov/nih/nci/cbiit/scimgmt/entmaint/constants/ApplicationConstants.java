6c34aafa9403b06778f6c42f77b18f83afd1ad85
tembharend <tembharend@nih.gov>
2015-03-26 16:13:15 +0000

    Added new constant

--- src/gov/nih/nci/cbiit/scimgmt/entmaint/constants/ApplicationConstants.java
+++ src/gov/nih/nci/cbiit/scimgmt/entmaint/constants/ApplicationConstants.java
@@ -55,6 +55,7 @@ public interface ApplicationConstants {
     public static final String YOUR_GRANT_DETAILS_APP_LINK_LIST = "yourGrantDetailsapplinklist";
     public static final String APP_LOOKUP_PORTFOLIO_CATEGORY_LIST = "appLookupPortfolioCategory";
     public static final String ORGANIZATION_DROPDOWN_LIST = "orgList";
+    public static final String CATEGORY_DROPDOWN_LIST = "categoryList";
     public static final String DISCREPANCY_TYPES_LIST = "discrepancyTypesList";
     public static final String ERA_ROLES_LIST = "eraBusinessRolesList";
     public static final String GLOBAL_LOOKUP_LIST = "appLookup";
