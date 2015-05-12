f8519b6ca0900ab30337e646df0ce980696241f5
menons2 <menons2@nih.gov>
2015-05-12 04:20:06 +0000

    Added placeholder action class for admin reports.

---
+++ src/gov/nih/nci/cbiit/scimgmt/entmaint/actions/AdminReportsAction.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gov.nih.nci.cbiit.scimgmt.entmaint.actions;
+
+import gov.nih.nci.cbiit.scimgmt.entmaint.constants.ApplicationConstants;
+
+
+public class AdminReportsAction extends BaseAction {
+
+	
+	
+public String execute() throws Exception {
+        
+    	//TBD
+        
+        return ApplicationConstants.SUCCESS;
+    }
+
+}
