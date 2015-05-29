cbdf39635f03bc8cec2fa1de45cfbee69926a2b9
zhoujim <zhoujim@nih.gov>
2015-05-29 13:58:54 +0000

    Make sure all name is using the VO.getFullname().

--- src/gov/nih/nci/cbiit/scimgmt/entmaint/actions/decorator/AuditSearchResultDecorator.java
+++ src/gov/nih/nci/cbiit/scimgmt/entmaint/actions/decorator/AuditSearchResultDecorator.java
@@ -52,12 +52,13 @@ public class AuditSearchResultDecorator extends TableDecorator{
 		AuditSearchVO searchVO = (AuditSearchVO)this.getPageContext().getSession().getAttribute(ApplicationConstants.SEARCHVO);
 		Long auditId = searchVO.getAuditId();
 		AuditAccountVO accountVO = (AuditAccountVO)getCurrentRowObject();
-		if(accountVO.getDeletedDate() != null){
-			if(accountVO.getFullName() != null && accountVO.getFullName().length() > 0){
-				name = accountVO.getFullName();
-			}else{
-				name = "";
-			}
+//		if(accountVO.getDeletedDate() != null){
+//			name = (accountVO.getImpaciiFirstName()==null? "" : accountVO.getImpaciiFirstName()) + " " + (accountVO.getImpaciiLastName()==null? "" : accountVO.getImpaciiLastName());		
+//		}else{
+//			name = (accountVO.getNedFirstName()==null? "" : accountVO.getNedFirstName()) + " " + (accountVO.getNedLastName()==null? "" : accountVO.getNedLastName());	
+//		}
+		if(accountVO.getFullName() != null && accountVO.getFullName().length() > 0){
+			name=accountVO.getFullName();
 		}
 		String id = ""+accountVO.getId();
 		String actionStr = "";
