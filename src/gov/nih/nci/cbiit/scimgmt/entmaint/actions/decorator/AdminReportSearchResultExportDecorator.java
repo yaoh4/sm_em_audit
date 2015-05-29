5294e9d79ab8b76271b0be9f2375f489c534cd1e
zhoujim <zhoujim@nih.gov>
2015-05-29 15:19:53 +0000

    Report is using impacii Last/First Name

--- src/gov/nih/nci/cbiit/scimgmt/entmaint/actions/decorator/AdminReportSearchResultExportDecorator.java
+++ src/gov/nih/nci/cbiit/scimgmt/entmaint/actions/decorator/AdminReportSearchResultExportDecorator.java
@@ -48,8 +48,18 @@ public class AdminReportSearchResultExportDecorator extends TableDecorator{
 	 */
 	public String getFullName(){
 		AuditAccountVO accountVO = (AuditAccountVO)getCurrentRowObject();
-		String fullName = accountVO.getFullName();
-		//String email = accountVO.getNedEmailAddress();
+		String lastName = accountVO.getImpaciiLastName();
+		String firstName = accountVO.getImpaciiFirstName();
+		String fullName = "";
+		if(lastName != null){
+			fullName = lastName;
+		}
+		if(lastName != null && lastName.length() > 0 && firstName != null && firstName.length() > 0){
+			fullName = fullName + ", ";
+		}
+		if(firstName != null){
+			fullName = fullName + firstName;
+		}
 		if(StringUtils.isBlank(fullName)){
 			return "";
 		}else{
