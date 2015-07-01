523f80dea18685ba60eaced3b13b8cc62704e87a
dinhys <dinhys@nih.gov>
2015-07-01 20:13:50 +0000

    Merged from trunk to v1.1 for ENTMAINT-167

--- src/gov/nih/nci/cbiit/scimgmt/entmaint/dao/Impac2AuditDAO.java
+++ src/gov/nih/nci/cbiit/scimgmt/entmaint/dao/Impac2AuditDAO.java
@@ -382,11 +382,11 @@ public class Impac2AuditDAO {
 		
 		// firstName partial search
 		if (!StringUtils.isBlank(searchVO.getUserFirstname())) {
-			criteria.add(Restrictions.ilike("nedFirstName", searchVO.getUserFirstname().trim(), MatchMode.START));
+			criteria.add(Restrictions.ilike("firstName", searchVO.getUserFirstname().trim(), MatchMode.START));
 		}
 		// lastName partial search
 		if (!StringUtils.isBlank(searchVO.getUserLastname())) {
-			criteria.add(Restrictions.ilike("nedLastName", searchVO.getUserLastname().trim(), MatchMode.START));
+			criteria.add(Restrictions.ilike("lastName", searchVO.getUserLastname().trim(), MatchMode.START));
 		}
 
 		// org
@@ -422,11 +422,11 @@ public class Impac2AuditDAO {
 		
 		// firstName partial search
 		if (!StringUtils.isBlank(searchVO.getUserFirstname())) {
-			criteria.add(Restrictions.ilike("nedFirstName", searchVO.getUserFirstname().trim(), MatchMode.START));
+			criteria.add(Restrictions.ilike("firstName", searchVO.getUserFirstname().trim(), MatchMode.START));
 		}
 		// lastName partial search
 		if (!StringUtils.isBlank(searchVO.getUserLastname())) {
-			criteria.add(Restrictions.ilike("nedLastName", searchVO.getUserLastname().trim(), MatchMode.START));
+			criteria.add(Restrictions.ilike("lastName", searchVO.getUserLastname().trim(), MatchMode.START));
 		}
 
 		// org
